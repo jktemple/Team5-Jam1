8fc75ad793f6e0235193162335814dfda0be198b
berrynul <blueorangerino@gmail.com>
2022-10-04 00:01:06 -0700

    Added Adjustable map movement and models
    -Added new models and movement to the map and cars using the RunnerObjectMove script

---
+++ Jam 1 Game/Assets/Jetpack runner/Scripts/RunnerRoadMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class RunnerObjectMove : MonoBehaviour
+{
+
+    public Vector3 velocity = new Vector3(-1.0f, 0f, 0f);
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + velocity;
+
+    }
+}
