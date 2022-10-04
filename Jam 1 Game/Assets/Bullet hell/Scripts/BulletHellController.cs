0574b4a3daac49642035ef4db8b0e5e16925591f
jktemple <jktemple@ucsc.edu>
2022-10-03 22:41:11 -0700

    Sheilding

--- Jam 1 Game/Assets/Bullet hell/Scripts/BulletHellController.cs
+++ Jam 1 Game/Assets/Bullet hell/Scripts/BulletHellController.cs
@@ -6,14 +6,25 @@ public class BulletHellController : MonoBehaviour
 {
     public float moveSpeed = 10;
     public float fireSpeed;
+    public float startingSheilds;
+    public float shields;
+    public float shieldDecay;
+    public float shieldCooldownTimer;
+    public float health;
     public GameObject bullet;
     float timer = 0;
     private Rigidbody2D shipRigidbody;
+    private bool isShielded;
+    private bool shieldCoolingdown = false;
+    public GameObject sheildSprite;
+    private SpriteRenderer shieldRenderer;
+    public float cooldownTimer;
     
     // Start is called before the first frame update
     void Start()
     {
         shipRigidbody = GetComponent<Rigidbody2D>();
+        shieldRenderer = sheildSprite.GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate(){
@@ -32,6 +43,29 @@ public class BulletHellController : MonoBehaviour
         
        // transform.Translate(new Vector3(-verticalInput,horizontalInput, 0) * moveSpeed * Time.deltaTime);
         
+        if(Input.GetAxis("Fire1") > 0 && shields > 0 && shieldCoolingdown == false){
+            Debug.Log("Shields Up");
+            isShielded = true;
+            shieldRenderer.enabled = true;
+            shields = shields - shieldDecay*Time.deltaTime;
+        } else {
+            isShielded = false;
+            shieldRenderer.enabled = false;
+        }
+
+        if(shields <= 0){
+            shieldCoolingdown = true;
+        }
+
+        if(shieldCoolingdown == true){
+            cooldownTimer+= Time.deltaTime;
+        }
+
+        if(cooldownTimer >= shieldCooldownTimer){
+            cooldownTimer = 0;
+            shields = startingSheilds;
+            shieldCoolingdown = false;
+        }
 
         if(Input.GetAxis("Jump") > 0 && timer > fireSpeed){
            // Debug.Log("Fire");
@@ -41,4 +75,14 @@ public class BulletHellController : MonoBehaviour
         timer += Time.deltaTime;
         //Debug.Log(timer);
     }
+
+    void takeDamage(float damage){
+        if(isShielded){
+            shields -= damage;
+        } else {
+            health -= damage;
+        }
+    }
+
+
 }
