7b0cc42d44d0f773d4442f9281d57f6946f8e705
jktemple <jktemple@ucsc.edu>
2022-10-03 21:51:53 -0700

    Bullet Collision and Killing the Enemy

--- Jam 1 Game/Assets/Bullet hell/Scripts/BulletHellController.cs
+++ Jam 1 Game/Assets/Bullet hell/Scripts/BulletHellController.cs
@@ -8,27 +8,33 @@ public class BulletHellController : MonoBehaviour
     public float fireSpeed;
     public GameObject bullet;
     float timer = 0;
+    private Rigidbody2D shipRigidbody;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipRigidbody = GetComponent<Rigidbody2D>();
     }
 
+    void FixedUpdate(){
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        shipRigidbody.MovePosition(transform.position + (new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime));
+        /*Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        shipRigidbody.MovePosition(Camera.main.ViewportToWorldPoint(pos));
+        */
+    }
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(-verticalInput,horizontalInput, 0) * moveSpeed * Time.deltaTime);
-        Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
-        pos.x = Mathf.Clamp01(pos.x);
-        pos.y = Mathf.Clamp01(pos.y);
-        transform.position = Camera.main.ViewportToWorldPoint(pos);
+        
+       // transform.Translate(new Vector3(-verticalInput,horizontalInput, 0) * moveSpeed * Time.deltaTime);
         
 
         if(Input.GetAxis("Jump") > 0 && timer > fireSpeed){
-            Debug.Log("Fire");
+           // Debug.Log("Fire");
             timer = 0;
             Instantiate(bullet, transform.position, Quaternion.identity);
         }
--- Jam 1 Game/Assets/Bullet hell/Scripts/ProjectileController.cs
+++ Jam 1 Game/Assets/Bullet hell/Scripts/ProjectileController.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
     public float moveSpeed;
+    public float damage;
+    private Rigidbody2D bulletRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(moveSpeed, 0 , 0) * moveSpeed * Time.deltaTime);
+        
         Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
         if(pos.x > 1 || pos.y > 1){
             Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate(){
+        bulletRigidbody.MovePosition(transform.position + new Vector3(moveSpeed, 0 , 0) * moveSpeed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collider){
+        //Debug.Log("Triggered");
+        if(collider.tag == "Enemy"){
+            Destroy(gameObject);
+            collider.gameObject.SendMessage("ApplyDamage", damage);
         }
     }
 }
