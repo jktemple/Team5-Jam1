568b4da6c1bab73572feb1f88808d1c6ecb3db74
LucianRain <aidanbacon@gmail.com>
2022-10-10 22:47:42 -0700

    working on transition scene and animations

--- Jam 1 Game/Assets/Main Menu/MenuManager.cs
+++ Jam 1 Game/Assets/Main Menu/MenuManager.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuManager : MonoBehaviour
 {
     public string StealthGameSceneName = "Stealth game";
+    public VideoPlayer player;
+    public GameObject videoTexture;
 
     void Start()
     {
-        
+        PlayerPrefs.SetInt("level", 0);
     }
 
     void Update()
     {
         
+    }
+
+    public void PlayCutscene1()
+    {
+        videoTexture.SetActive(true);
+        player.Play();
+        //wait 9.5 seconds
+        //SceneManager.LoadScene(StealthGameSceneName);
     }
 }
