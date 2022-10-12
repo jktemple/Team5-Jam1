d887efc9fef6643a4e36c7ffc211a6f6b555ea13
berrynul <blueorangerino@gmail.com>
2022-10-11 17:28:04 -0700

    Implemented food orbs, changed the organization around
    -Food orbs
    -Script organization
    -Started on UI scripts

---
+++ Jam 1 Game/Assets/Jetpack runner/Scripts/RunnerFoodManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerFoodManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter() => Destroy(gameObject); 
+
+
+    //from here I would want to communicate something to the UI, either using a observer pattern, or a singleton
+
+
+}
