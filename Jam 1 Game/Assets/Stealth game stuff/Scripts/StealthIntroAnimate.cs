4e57418f46d0f0c31a6faf434ed260e60df975ad
LucianRain <aidanbacon@gmail.com>
2022-10-12 22:28:10 -0700

    tmp adding knight animations

--- Jam 1 Game/Assets/Stealth game stuff/Scripts/StealthIntroAnimate.cs
+++ Jam 1 Game/Assets/Stealth game stuff/Scripts/StealthIntroAnimate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StealthInfoAnimate : MonoBehaviour
+public class StealthIntroAnimate : MonoBehaviour
 {
     public float speed;
     public float distance;
